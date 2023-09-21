f13abefe093ab9243c6154b66e955733159ec47b
Matthew Otto <matotto@okstate.edu>
2023-09-21 11:07:53 -0500

    update TAP controller

--- RTL/tap_controller.sv
+++ RTL/tap_controller.sv
@@ -1,27 +1,38 @@
 module tap_controller (
     input tck, trst, tms,
-    output [3:0] out
+    output logic reset,
+    output logic tdo_en,
+    output logic shiftIR,
+    output logic captureIR,
+    output logic clockIR,
+    output logic updateIR,
+    output logic shiftDR,
+    output logic captureDR,
+    output logic clockDR,
+    output logic updateDR,
+    output logic updateDRstate,
+    output logic select
 );
 
 logic [3:0] state;
 
 localparam [3:0]
-    tl_reset     = 4'b0000,
-    runtest_idle = 4'b0001,
-    select_dr    = 4'b0010,
-    capture_dr   = 4'b0011,
-    shift_dr     = 4'b0100,
-    exit1_dr     = 4'b0101,
-    pause_dr     = 4'b0110,
-    exit2_dr     = 4'b0111,
-    update_dr    = 4'b1000,
-    select_ir    = 4'b1001,
-    capture_ir   = 4'b1010,
-    shift_ir     = 4'b1011,
-    exit1_ir     = 4'b1100,
-    pause_ir     = 4'b1101,
-    exit2_ir     = 4'b1110,
-    update_ir    = 4'b1111;
+    exit2_dr     = 4'b0000,
+    exit1_dr     = 4'b0001,
+    shift_dr     = 4'b0010,
+    pause_dr     = 4'b0011,
+    select_ir    = 4'b0100,
+    update_dr    = 4'b0101,
+    capture_dr   = 4'b0110,
+    select_dr    = 4'b0111,
+    exit2_ir     = 4'b1000,
+    exit1_ir     = 4'b1001,
+    shift_ir     = 4'b1010,
+    pause_ir     = 4'b1011,
+    runtest_idle = 4'b1100,
+    update_ir    = 4'b1101,
+    capture_ir   = 4'b1110,
+    tl_reset     = 4'b1111;
 
 
 
@@ -30,101 +41,111 @@ always @(posedge tck, negedge trst) begin
         state <= tl_reset;
     else begin
         case (state)
-            tl_reset:
+            tl_reset: begin
+                reset <= 1'b1;
+                select <= 1'b1;
                 if (tms)
                     state <= tl_reset;
                 else
                     state <= runtest_idle;
-
-            runtest_idle:
+            end
+            runtest_idle: begin
+                reset <= 1'b0;
                 if (tms)
                     state <= select_dr;
                 else
                     state <= runtest_idle;
-
-            select_dr:
+            end
+            select_dr: begin
+                select <= 1'b0;
                 if (tms)
                     state <= select_ir;
                 else
                     state <= capture_dr;
-            
-            capture_dr:
+            end
+            capture_dr: begin
                 if (tms)
                     state <= exit1_dr;
                 else
                     state <= shift_dr;
-
-            shift_dr:
+            end
+            shift_dr: begin
+                tdo_en <= 1'b1;
                 if (tms)
                     state <= exit1_dr;
                 else
                     state <= shift_dr;
-
-            exit1_dr:
+            end
+            exit1_dr: begin
+                tdo_en <= 1'b0;
                 if (tms)
                     state <= update_dr;
                 else
                     state <= pause_dr;
-
-            pause_dr:
+            end
+            pause_dr: begin
                 if (tms)
                     state <= exit2_dr;
                 else
                     state <= pause_dr;
-
-            exit2_dr:
+            end
+            exit2_dr: begin
                 if (tms)
                     state <= update_dr;
                 else
                     state <= shift_dr;
-
-            update_dr:
+            end
+            update_dr: begin
                 if (tms)
                     state <= select_dr;
                 else
                     state <= runtest_idle;
-
-            select_ir:
+            end
+            select_ir: begin
                 if (tms)
                     state <= tl_reset;
                 else
                     state <= capture_ir;
-
-            capture_ir:
+            end
+            capture_ir: begin
+                select <= 1'b1;
                 if (tms)
                     state <= exit1_ir;
                 else
                     state <= shift_ir;
-
-            shift_ir:
+            end
+            shift_ir: begin
+                tdo_en <= 1'b1;
                 if (tms)
                     state <= exit1_ir;
                 else
                     state <= shift_ir;
-
-            exit1_ir:
+            end
+            exit1_ir: begin
+                tdo_en <= 1'b0;
                 if (tms)
                     state <= update_ir;
                 else
                     state <= pause_ir;
-
-            pause_ir:
+            end
+            pause_ir: begin
                 if (tms)
                     state <= exit2_ir;
                 else
                     state <= pause_ir;
-
-            exit2_ir:
+            end
+            exit2_ir: begin
                 if (tms)
                     state <= update_ir;
                 else
                     state <= shift_ir;
-
-            update_ir:
+            end
+            update_ir: begin
                 if (tms)
                     state <= select_dr;
                 else
                     state <= runtest_idle;
+            end
         endcase
     end // else
 end
